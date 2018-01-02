b012ab3fa9b6bc619a08f8eb9a7d9a491540c148
Bhupendra Singh <bhup7060@gmail.com>
2018-01-02 15:50:21 +0530

    Testing the Queue container adapter

--- Melody-Sigma/main.cpp
+++ Melody-Sigma/main.cpp
@@ -3,6 +3,7 @@
 #include <Command/RealCommand.h>
 #include <Algorithm/Sorting.h>
 #include <Algorithm/TowerOfHanoi.h>
+#include <queue>
 
 using namespace std;
 
@@ -25,31 +26,6 @@ int main()
         cout << "Invalid command mode" << endl;
     }
 
-
-
-    /*Queue testing
-    queue<int> QUE;
-    //int ele; //element
-    int n=1;
-    //push operation
-    //Through loop
-    //do{
-    //  cin>>ele;
-    // QUE.push(ele);
-    // n++;
-    //}while(n<=5);
-    //manually pushing each element
-    QUE.push(10);
-    QUE.push(20);
-    QUE.push(30);
-    QUE.push(40);
-    QUE.push(50);
-    //traverse and pop
-    while(!QUE.empty()){
-        cout<<QUE.front()<<"  ";
-        QUE.pop();
-    }
-    */
     //grouped files test
     //Sorting test
     int a[10]={40, 60, 10, 20, 45, 70, 80, 90, 25, 15};
@@ -63,4 +39,28 @@ int main()
     //Tower of Hanoi test
     TowerOfHanoi obj2;
     obj2.tower(4, 'A', 'B', 'C');
+}
+*/
+    //Queue testing
+    queue<int> QUE;
+    //int ele; //element
+    //push operation
+    //Through loop
+    /*do{
+      cin>>ele;
+      QUE.push(ele);
+      n++;
+    }while(n<=5);
+    */
+    //manually pushing each element
+    QUE.push(10);
+    QUE.push(20);
+    QUE.push(30);
+    QUE.push(40);
+    QUE.push(50);
+    //traverse and pop
+    while(!QUE.empty()){
+        cout<<QUE.front()<<"  ";
+        QUE.pop();
+    }
 }
